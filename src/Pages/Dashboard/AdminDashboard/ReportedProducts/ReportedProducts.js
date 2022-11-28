10b226f7eaef7a023a5073ff37ae8457cab2c143
GHSoyad <ghsoyad@gmail.com>
2022-11-28 13:13:10 +0600

    Added reported products delete method

--- src/Pages/Dashboard/AdminDashboard/ReportedProducts/ReportedProducts.js
+++ src/Pages/Dashboard/AdminDashboard/ReportedProducts/ReportedProducts.js
@@ -1,11 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import ConfirmModal from '../../../../Components/ConfirmModal/ConfirmModal';
 import Loader from '../../../../Components/Loader/Loader';
 
 const ReportedProducts = () => {
 
-    const { isLoading, data: reportedProducts } = useQuery({
+    const [productInfo, setProductInfo] = useState(null);
+
+    const { isLoading, data: reportedProducts, refetch } = useQuery({
         queryKey: ['reportedProducts'],
         queryFn: () => axios.get('http://localhost:5000/report/products', {
             headers: {
@@ -15,9 +19,27 @@ const ReportedProducts = () => {
             .then(data => data.data)
     })
 
+    const handleDelete = (product) => {
+        const productId = product.productId;
+
+        fetch(`http://localhost:5000/report/products/${productId}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('cadenceSecretToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    toast.success('Product deleted!');
+                    refetch();
+                }
+            })
+    }
+
     return (
         <div>
-            <h2 className='text-center text-2xl font-medium pb-4'>All Sellers</h2>
+            <h2 className='text-center text-2xl font-medium pb-4'>Reported Products</h2>
             {
                 isLoading ?
                     <Loader>Loading Reported Products...</Loader>
@@ -41,12 +63,18 @@ const ReportedProducts = () => {
                                             <td>{product.product}</td>
                                             <td>{product.reporter}</td>
                                             <td>{product.sellerEmail}</td>
-                                            <td className='text-center py-0'><label htmlFor="confirm-modal" className='btn btn-xs hover:glass'>Delete</label></td>
+                                            <td className='text-center py-0'><label onClick={() => setProductInfo(product)} htmlFor="confirm-modal" className='btn btn-xs hover:glass'>Delete</label></td>
                                         </tr>
                                     </tbody>)
                             }
                         </table>
-
+                        {
+                            productInfo &&
+                            <ConfirmModal modalData={productInfo} closeModal={setProductInfo} confirm={handleDelete}>
+                                <h3 className="font-bold text-lg">Delete Seller?</h3>
+                                <p className="py-4"><span className='font-bold text-red-500'>"{productInfo.name}"</span> will be deleted permanently!</p>
+                            </ConfirmModal>
+                        }
                     </div>
             }
         </div>
